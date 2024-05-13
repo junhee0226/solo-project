d6ddb6bef592fbe11781b20cd4551be53bc93811
junhee0226 <167046265+junhee0226@users.noreply.github.com>
2024-05-14 01:52:54 +0900

    JHL_Player Movement
    직접적인 캐릭터의 움직임+키보드 좌우를 Scale값을 건드려 좌우 반전은 주었음+ 직접 캐릭터 속도 조절 가능

---
+++ PlayerInputController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : TopDownController
+{
+    private Camera camera;
+    //Scale 값을 건드려 캐릭터 이동방향에 따른 좌우반전은 했는데 마우스 포인트를 따라가는건 모르겠음!
+    [SerializeField][Range(1f, 10f)] float moveSpeed = 1f;
+
+    private void Update()
+    {
+        Vector3 flipMove = Vector3.zero;
+
+        if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            flipMove = Vector3.left;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        else if(Input.GetAxisRaw("Horizontal") > 0) 
+        { 
+            flipMove = Vector3.right;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        transform.position += flipMove * moveSpeed * Time.deltaTime;
+    }
+
+    private void Awake()
+    {
+        camera = Camera.main;
+    }
+    
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+        CallMoveEvent(moveInput);
+    }
+
+    public void ONLook(InputValue value)
+    {
+        Vector2 newAim = value.Get<Vector2>();
+        Vector2 worldPos = camera.ScreenToWorldPoint(newAim);
+        newAim = (worldPos = (Vector2)transform.position).normalized;
+
+        CallLookEvent(newAim);
+    }
+}
